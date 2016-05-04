083d5a1042e94bceb25f205d27c993bb43831714
Yoni Pinto <yoni.b.pinto@gmail.com>
2016-05-03 19:21:51 -0500

    getting rid of supersonic stuff

--- app/common/index.js
+++
@@ -1,4 +0,0 @@
-angular.module('common', [
-  // Declare here all AngularJS dependencies that are shared by all modules.
-  'supersonic'
-]);
--- app/example/index.js
+++
@@ -1,4 +0,0 @@
-angular.module('example', [
-  // Declare here all AngularJS dependencies that are shared by the example module.
-  'supersonic'
-]);
