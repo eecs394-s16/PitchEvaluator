273f3030b72e19c22ecc32177b21d1bc3527020c
dinomujki <dinomujkic2017@u.northwestern.edu>
2016-05-05 11:39:45 -0500

    Improved how routing is done, things much cleaner now

--- app/app.js
+++ app/app.js
@@ -1,13 +1,18 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version',
-  'firebase'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+angular
+  .module('PitchEvaluator', ['ngRoute'])
+  .config(function($routeProvider) {
+  $routeProvider
+    .when('/view1', {
+      templateUrl: 'view1/view1.html',
+      controller: 'View1Ctrl'
+    })
+    .when('/view2', {
+      templateUrl: 'view2/view2.html',
+      controller: 'View2Ctrl'
+    })
+    .otherwise({
+      redirectTo: 'view1'
+    });
+});
--- app/view2/view2.js
+++ app/view2/view2.js
@@ -1,14 +1,6 @@
 'use strict';
+angular
+  .module('PitchEvaluator')
+  .controller('View2Ctrl', function() {
 
-angular.module('myApp.view2', ['ngRoute'])
-
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view2', {
-    templateUrl: 'view2/view2.html',
-    controller: 'View2Ctrl'
-  });
-}])
-
-.controller('View2Ctrl', [function() {
-
-}]);
+  })
