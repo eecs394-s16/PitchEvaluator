bf294bf42230165ca6631ff245b43ffd073ea30b
Yoni Pinto <yoni.b.pinto@gmail.com>
2016-05-09 14:18:24 -0500

    cleaning up view2 controller

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -3,64 +3,48 @@ angular
   .module('PitchEvaluator')
   .controller('View2Ctrl', function($scope, $firebaseObject, $firebaseArray) {
   	var teamsRef = new Firebase("https://pitchevaluator.firebaseio.com/teams");
-  	//Pulling the FirebaseArray into scope
-	$scope.teamList = $firebaseArray(teamsRef);
 
-	$scope.curTeamName;
-	$scope.curTeamIndex;
-	$scope.curTeamKey;
-	var curTeamObject;
-	var curTeamRef;
-	$scope.q1;
-	$scope.q2;
-	$scope.q3;
-	$scope.q4;
+	var teamList = $firebaseArray(teamsRef);
+	$scope.teams = teamList;
+
+	var curTeamName, curTeamIndex, curTeamKey, curTeamObject, curTeamRef;
+	var q1, q2, q3, q4, cmt1, cmt2, cmt3, cmt4;
 
 	$scope.onSubmit = function() {
-		$scope.curTeamName = document.getElementById("team-select").value;
-		// console.log($scope.curteamList;
-		$scope.q1 = $('input[name="q1radio"]:checked').val();
-		$scope.q2 = $('input[name="q2radio"]:checked').val();
-		$scope.q3 = $('input[name="q3radio"]:checked').val();
-		$scope.q4 = $('input[name="q4radio"]:checked').val();
-
-		$scope.cmt1 = document.getElementById("q1textarea").value;
-		$scope.cmt2 = document.getElementById("q2textarea").value;
-		$scope.cmt3 = document.getElementById("q3textarea").value;
-		$scope.cmt4 = document.getElementById("q4textarea").value;
+		curTeamName = document.getElementById("team-select").value;
 		
-		for (var i = 0; i < $scope.teamList.length; i++) {
-			if ($scope.teamList[i].name == $scope.curTeamName) {
-				$scope.curTeamIndex = i;
-				//console.log(i);
-				$scope.curTeamKey = $scope.teamList.$keyAt($scope.curTeamIndex);
-				var curTeamObject = $firebaseObject(teamsRef.child($scope.curTeamKey));
+		q1 = $('input[name="q1radio"]:checked').val();
+		q2 = $('input[name="q2radio"]:checked').val();
+		q3 = $('input[name="q3radio"]:checked').val();
+		q4 = $('input[name="q4radio"]:checked').val();
 
-		//Updating a Team:
-			//Create a reference to the team:
-				curTeamRef = teamsRef.child($scope.curTeamKey);
-			//Utilize the "update()" function
-				curTeamRef.update({ q1Val: $scope.q1 });
-//				curTeamRef.update({ q2Val: $scope.q2 });
-				curTeamRef.update({ q1Comment: $scope.cmt1, q2Comment: $scope.cmt2, q3Comment: $scope.cmt3, q4Comment: $scope.cmt4});
+		cmt1 = document.getElementById("q1textarea").value;
+		cmt2 = document.getElementById("q2textarea").value;
+		cmt3 = document.getElementById("q3textarea").value;
+		cmt4 = document.getElementById("q4textarea").value;
+		
+		for (var i = 0; i < teamList.length; i++) {
+			if (teamList[i].name == curTeamName) {
+				curTeamIndex = i;
+				curTeamKey = teamList.$keyAt(curTeamIndex);
+				curTeamObject = $firebaseObject(teamsRef.child(curTeamKey))
+			//Updating a Team:
+				//Create a reference to the team:
+				curTeamRef = teamsRef.child(curTeamKey);
 
-	// *****OLD/FLAWED Data Binding
-				/*curTeamObject.$bindTo($scope, "curTeam").then(function() {
-				//console.log($scope.curTeam);
-				$scope.curTeam.q1Comment = $scope.cmt1;
-				$scope.curTeam.q2Comment = $scope.cmt2;
-				$scope.curTeam.q3Comment = $scope.cmt3;
-				$scope.curTeam.q4Comment = $scope.cmt4;
-				$scope.curTeam.q1Val = $scope.q1;
-				$scope.curTeam.q2Val = $scope.q2;
-				$scope.curTeam.q3Val = $scope.q3;
-				$scope.curTeam.q4Val = $scope.q4;
-			});*/
+				//Utilize the "update()" function
+				curTeamRef.update({ q1Val: q1,
+									q1cmt: cmt1,
+									q2Val: q2,
+									q2cmt: cmt2,
+									q3Val: q3,
+									q3cmt: cmt3,
+									q4Val: q4,
+									q4cmt: cmt4
+								 });
 			}
 		};
-		//$scope.curTeamObject = $scope.teamList.$getRecord($scope.curTeamKey);
-//		console.log($scope.curTeamObject);
-		// console.log(document.getElementById("q1textarea").value);
+
 	};
 	
 
