3dc176a156a112e981f11ab68cc18bb26895d45b
Sarah(Baiyu) Yang <ybywmr@163.com>
2016-05-11 14:29:19 -0500

    Fix highlight active tab for current view

--- app/TabsCtrl.js
+++ app/TabsCtrl.js
@@ -3,22 +3,29 @@ angular
 .module('PitchEvaluator')
 .controller('TabsCtrl', function($scope, $location) {
 	$scope.tabs = [
-      { link : '#/view1', label : 'View1' },
-      { link : '#/view2', label : 'View2' },
-      { link : '#/formBuilder', label : 'formBuilder' }
+      { link : '/view1', label : 'View1' },
+      { link : '/view2', label : 'View2' },
+      { link : '/formBuilder', label : 'formBuilder' }
     ]; 
     
-  $scope.selectedTab = $scope.tabs[0];    
-  $scope.setSelectedTab = function(tab) {
-    $scope.selectedTab = tab;
-  }
+  // $scope.selectedTab = $scope.tabs[0];    
+  // $scope.setSelectedTab = function(tab) {
+  //   $scope.selectedTab = tab;
+  // }
   
-  $scope.tabClass = function(tab) {
-    if ($scope.selectedTab == tab) {
-      return "active";
-    } else {
-      return "";
-    }
-    console.log(tab);
-  }
+  // $scope.tabClass = function(tab) {
+  //   if ($scope.selectedTab == tab) {
+  //     return "active";
+  //   } else {
+  //     return "";
+  //   }
+  //   console.log(tab);
+  // }
+
+  $scope.isActive = function(route) {
+        return route === $location.path();
+        console.log(route);
+    };
+
+
  });
