a317e0dfa0531c1eaa01397573d2c14dc2421e9a
dinomujki <dinomujkic2017@u.northwestern.edu>
2016-05-14 10:43:44 -0500

    review object

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -2,10 +2,14 @@
 angular
 .module('PitchEvaluator')
 .controller('View2Ctrl', function($scope, $firebaseObject, $firebaseArray, $location) {
-  	var teamsRef = new Firebase("https://pitchevaluator.firebaseio.com/teams");
-
+	var teamsRef = new Firebase("https://pitchevaluator.firebaseio.com/teams");
+  $scope.loadingTeams = true;
 	var teamList = $firebaseArray(teamsRef);
-	$scope.teams = teamList;
+  console.log(teamList);
+	teamList.$loaded(function() {
+    $scope.loadingTeams = false;
+    $scope.teams = teamList;
+  });
 
 	var curTeamName, curTeamIndex, curTeamKey, curTeamObject, curTeamRef;
 	var q1, q2, q3, q4, q5, cmt1, cmt2, cmt3, cmt4, cmt5;
@@ -37,10 +41,10 @@ angular
 		}
 		if (cmt4 != "") {
 			team.update({q4cmt: cmt4});
-		}										
+		}
 		if (cmt5 != "") {
 			team.update({q5cmt: cmt5});
-		}	
+		}
 		if (q1 != undefined && q2 != undefined && q3 != undefined & q4 != undefined) {
 			var teamavg = (parseFloat(q1) + parseFloat(q2) + parseFloat(q3) + parseFloat(q4))/4.0;
 			teamavg = teamavg.toFixed(2);
@@ -48,33 +52,68 @@ angular
 		}
 	}
 
+  class Evaluation {
+    constructor(user, teamName, q1, cmt1, q2, cmt2, q3, cmt3, q4, cmt4, q5, cmt5) {
+      this.user = user;
+      this.teamName = teamName;
+      this.q1 = q1;
+      this.cmt1 = cmt1;
+      this.q2 = q2;
+      this.cmt2 = cmt2;
+      this.q3 = q3;
+      this.cmt3 = cmt3;
+      this.q4 = q4;
+      this.cmt4 = cmt4;
+      this.q5 = q5;
+      this.cmt5 = cmt5;
+    }
+  }
 	$scope.onSubmit = function() {
 		curTeamName = document.getElementById("team-select").value;
-		
+
 		q1 = $('input[name="q1radio"]:checked').val();
 		q2 = $('input[name="q2radio"]:checked').val();
 		q3 = $('input[name="q3radio"]:checked').val();
 		q4 = $('input[name="q4radio"]:checked').val();
-		q5 = $('input[name="q5radio"]:checked').val();		
+		q5 = $('input[name="q5radio"]:checked').val();
 
 		cmt1 = document.getElementById("q1textarea").value;
 		cmt2 = document.getElementById("q2textarea").value;
 		cmt3 = document.getElementById("q3textarea").value;
 		cmt4 = document.getElementById("q4textarea").value;
 		cmt5 = document.getElementById("q5textarea").value;
-		
-		for (var i = 0; i < teamList.length; i++) {
+
+		// for (var i = 0; i < teamList.length; i++) {
+		// 	if (teamList[i].name == curTeamName) {
+		// 		curTeamIndex = i;
+		// 		curTeamKey = teamList.$keyAt(curTeamIndex);
+		// 		curTeamObject = $firebaseObject(teamsRef.child(curTeamKey))
+    //
+		// 		curTeamRef = teamsRef.child(curTeamKey);
+    //
+		// 		dbUpdate(curTeamRef, q1, cmt1, q2, cmt2, q3, cmt3, q4, cmt4, q5, cmt5);
+		// 	}
+		// }
+
+    //@TODO: implemented user auth
+    var user = "John Doe"
+
+    var evaluation = new Evaluation(user, curTeamName, q1, cmt1, q2, cmt2, q3, cmt3, q4, cmt4, q5, cmt5);
+    console.log(evaluation);
+    for (var i = 0; i < teamList.length; i++) {
 			if (teamList[i].name == curTeamName) {
-				curTeamIndex = i;
-				curTeamKey = teamList.$keyAt(curTeamIndex);
-				curTeamObject = $firebaseObject(teamsRef.child(curTeamKey))
-			
-				curTeamRef = teamsRef.child(curTeamKey);
+        var teamRef = teamsRef + "/" + teamList[i].$id;
+        var team = new Firebase(teamRef);
+        if (team.reviews) {
+          console.log('Woot');
+        }
+        else {
+          var reviews = team.child('reviews');
+          reviews.push(evaluation);
+        }
+      }
+    }
 
-				dbUpdate(curTeamRef, q1, cmt1, q2, cmt2, q3, cmt3, q4, cmt4, q5, cmt5);
-			}
-		}
-
-      $location.path('#/view1');
+    // $location.path('#/view1');
 	};
 });
