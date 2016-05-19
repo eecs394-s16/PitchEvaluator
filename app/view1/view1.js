89d1144a4b0ca07cea38d48a06a4fdf108796567
Sarah(Baiyu) Yang <ybywmr@163.com>
2016-05-19 00:21:35 -0500

    conflicts in view1.js fixed

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,77 +9,11 @@ angular
       $scope.teamList.sort(function(a,b) {return a.rank-b.rank});
     })
 
-<<<<<<< Updated upstream
     $scope.saveTeam = function(teamName) {
       teamService.set(teamName);
       $location.path('view2');
     }
 
-
-  	$scope.changeRanking = function(id, valueStr) {
-      var value = Number(valueStr);
-      var index;
-      for (let i=0; i<$scope.teamList.length;i++) {
-        if ($scope.teamList[i].$id==id) {
-          index=i;
-=======
- //  	$scope.changeRanking = function(id, valueStr) {
- //      var value = Number(valueStr);
- //      var index;
- //      for (let i=0; i<$scope.teamList.length;i++) {
- //        if ($scope.teamList[i].$id==id) {
- //          index=i;
- //        }
- //        // console.log($scope.teamList[i].name, $scope.teamList[i].rank);
- //      }
- //      if (value>0 && value<=$scope.teamList.length) {
- //        console.log($scope.teamList);
-
- //        if (index<value-1) {
- //          // console.log("First case");
- //          // console.log("index",index);
- //          // console.log("value",value);
- //          $scope.teamList[index].rank = value;
- //          for (let i=index+1; i<value;i++) {
- //            $scope.teamList[i].rank-=1;
- //          }
- //        }
- //        else {
- //          // console.log("Second case");
- //          // console.log("index",index);
- //          // console.log("value",value);
- //          $scope.teamList[index].rank = value;
- //          for (let i=value-1; i<index;i++) {
- //            $scope.teamList[i].rank+=1;
- //          }
- //        }
-
- //        for (let i=0; i<$scope.teamList.length;i++) {
- //          $scope.teamList.$save(i);
- //        }
- //        $scope.teamList.sort(function(a,b) {return a.rank-b.rank})
- //        // console.log($scope.teamList);
- //      }
- //      else {
- //        console.log('Invalid rank value');
- //        $scope.teamList[index].rank = index+1;
- //      }
- //    }
-
-	// // var selectedTeam = teamsRef + "/" + teamList[i].$id;
- //    $scope.allowDrop = function(ev) {
- //      ev.preventDefault();
- //    }
- //    $scope.drag = function(ev) {
- //      console.log(ev)
- //      ev.dataTransfer.setData("text", ev.target.id);
- //    }
- //  })
-
-
-    
-
-    // $scope.teamList.$loaded(function() {
 
       $scope.dragStart = function(e, ui) {
         ui.item.data('start', ui.item.index());
@@ -94,7 +28,7 @@ angular
         console.log('end=',end);
         for (let i=0; i<$scope.teamList.length;i++) {
           console.log($scope.teamList[i].name, $scope.teamList[i].rank);
->>>>>>> Stashed changes
+
         }
 
         if (start<end) {
