214b50ea81d9cf3f05d96ed66d004a0a79a05399
strippy16 <samanthatrippy2016@u.northwestern.edu>
2016-05-25 23:33:33 -0500

    Finished Download-to-CSV-Button on Overview Page

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,8 @@ angular
   	// 	$location.path('login')
   	// }
 
+    var teamsForCSV = [];
+
     var sessListRef = new Firebase(db_url+"/sessionList");
     var temp = new $firebaseArray(sessListRef);
     temp.$loaded(function() {
@@ -17,16 +19,20 @@ angular
           $scope.teamList = $firebaseArray(new Firebase(session.ref+"/teams"));
           $scope.teamList.$loaded(function() {
             $scope.teamList.sort(function(a,b) {return a.rank-b.rank});
+
+            // to create the arrayOfObjects you'll print to CSV
+            for (var i = 0; i < $scope.teamList.length; i++) {
+              var curTeam = $scope.teamList[i];
+              var teamy = new Team(curTeam.name, curTeam.q1Val, 
+                curTeam.q2Val, curTeam.q3Val, curTeam.q4Val, 
+                curTeam.ovrAvg, curTeam.rank);
+              teamsForCSV.push(teamy);
+            }//end for loop
+
           })
         }
       });
     });
-
-  	// $scope.teamList = $firebaseArray(teamsRef);
-    // $scope.teamList.$loaded(function() {
-    //   $scope.teamList.sort(function(a,b) {return a.rank-b.rank});
-    // })
-
 
     //Function to store the team in the teamService
     $scope.saveTeam = function(teamName) {
@@ -72,5 +78,74 @@ angular
       start: $scope.dragStart,
       update: $scope.dragEnd
     });
+
+    //sam's Download Team Data CSV
+
+    class Team {
+      constructor(ToP, PoN, Demo, CI, Business, TA, rank) {
+        this.Team_or_Product = ToP;
+          this.Problem_or_Need = PoN;
+          this.Demo = Demo;
+          this.Customer_Insight = CI;
+          this.Business = Business;
+          this.Team_Average = TA;
+          this.Rank = rank
+      }
+  }
+
+    function convertArrayOfObjectsToCSV(args) {
+        var result, ctr, keys, columnDelimiter, lineDelimiter, data;
+
+        data = args.data || null;
+        if (data == null || !data.length) {
+            return null;
+        }
+
+        columnDelimiter = args.columnDelimiter || ',';
+        lineDelimiter = args.lineDelimiter || '\n';
+
+        keys = Object.keys(data[0]);
+
+        result = '';
+        result += keys.join(columnDelimiter);
+        result += lineDelimiter;
+
+        data.forEach(function(item) {
+            ctr = 0;
+            keys.forEach(function(key) {
+                if (ctr > 0) result += columnDelimiter;
+
+                result += item[key];
+                ctr++;
+            });
+            result += lineDelimiter;
+        });
+
+        return result;
+    }
+    $scope.downloadCSV = function(args) {
+        var data, filename, link;
+
+        var csv = convertArrayOfObjectsToCSV({
+            data: teamsForCSV
+        });
+        if (csv == null)
+            return;
+
+        filename = args.filename || 'export.csv';
+
+        if (!csv.match(/^data:text\/csv/i)) {
+            csv = 'data:text/csv;charset=utf-8,' + csv;
+        }
+        data = encodeURI(csv);
+
+        link = document.createElement('a');
+        link.setAttribute('href', data);
+        link.setAttribute('download', filename);
+ 
+        document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    }
 
 });
