e36463357acc911505d6f51d2014df74ba53f538
dinomujki <dinomujkic2017@u.northwestern.edu>
2016-05-30 17:25:03 -0500

    Update for permissions

--- app/services/permissions.js
+++ app/services/permissions.js
@@ -3,10 +3,11 @@ angular
   .factory('permissionsService', function($rootScope) {
     function isPermitted(action) {
       if ($rootScope.role == 'Admin') {
-        return true;
+        if (action=='Review') return false;
+        else return true;
       }
       else if ($rootScope.role == 'Judge') {
-        if (action=='JudgeView' || action=='Review') {
+        if (action=='JudgeView' || action=='Review' || action=="TeamView") {
           return true;
         }
         else return false;
