bae777cf1a4b2bce90dca8ce5ccffec537f839b1
Yoni Pinto <yoni.b.pinto@gmail.com>
2016-05-30 02:24:30 -0500

    changed http server deployment to fix heroku

---
+++ server.js
@@ -0,0 +1,9 @@
+var port = Number(process.env.PORT || 8000);
+
+var express = require('express'); var app = express();
+
+app.use(express.static(__dirname + '/app')); 
+
+var server = app.listen(port, function() 
+	{ console.log('Listening on port %d', server.address().port); 
+});
