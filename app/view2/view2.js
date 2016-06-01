2118234024af751794fea91b9975127121128b36
tz0531 <tz0531@gmail.com>
2016-05-31 20:38:02 -0500

    Updated View2 to add new questions

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -10,7 +10,7 @@ angular
 	}
 
 	$scope.toggleText= [];
-	for (var i=0; i<6; i++) {
+	for (var i=0; i<9; i++) {
 		$scope.toggleText.push('Add Comment');
 	}
 
@@ -23,7 +23,7 @@ angular
 	});
 	$scope.selectedTeam=null;
 	var curTeamName, curTeamIndex, curTeamKey, curTeamObject, curTeamRef;
-	var q1, q2, q3, q4, q5, cmt1, cmt2, cmt3, cmt4, cmt5;
+	var q1, q2, q3, q4, q5, q6, q7, q8, cmt1, cmt2, cmt3, cmt4, cmt5, cmt6, cmt7, cmt8;
 	var graderName, teamCheck;
 	var valid = false;
 
@@ -45,7 +45,10 @@ angular
 		2: false,
 		3: false,
 		4: false,
-		5: false
+		5: false,
+		6: false,
+		7: false,
+		8: false,
 	}
 	$scope.checkComment = function(num) {
 		return commentShowings[num];
@@ -89,33 +92,44 @@ angular
 		$("#q2slider").slider( "option", "value", 0 );
 		$("#q3slider").slider( "option", "value", 0 );
 		$("#q4slider").slider( "option", "value", 0 );
+		$("#q5slider").slider( "option", "value", 0 );
+		$("#q6slider").slider( "option", "value", 0 );
+		$("#q7slider").slider( "option", "value", 0 );
 
-		$('input[name="q5radio"]').val([null]);
+		$('input[name="q8radio"]').val([null]);
 
 		document.getElementById("q1textarea").value = null;
 		document.getElementById("q2textarea").value = null;
 		document.getElementById("q3textarea").value = null;
 		document.getElementById("q4textarea").value = null;
 		document.getElementById("q5textarea").value = null;
-
+		document.getElementById("q6textarea").value = null;
+		document.getElementById("q7textarea").value = null;
+		document.getElementById("q8textarea").value = null;
 	}
 	function updateValues(review) {
 		$("#q1slider").slider( "option", "value", review.q1 );
 		$("#q2slider").slider( "option", "value", review.q2 );
 		$("#q3slider").slider( "option", "value", review.q3 );
 		$("#q4slider").slider( "option", "value", review.q4 );
+		$("#q5slider").slider( "option", "value", review.q5 );
+		$("#q6slider").slider( "option", "value", review.q6 );
+		$("#q7slider").slider( "option", "value", review.q7 );
 
-		$('input[name="q5radio"]').val([review.q5]);
+		$('input[name="q8radio"]').val([review.q8]);
 
 		document.getElementById("q1textarea").value = review.cmt1;
 		document.getElementById("q2textarea").value = review.cmt2;
 		document.getElementById("q3textarea").value = review.cmt3;
 		document.getElementById("q4textarea").value = review.cmt4;
 		document.getElementById("q5textarea").value = review.cmt5;
+		document.getElementById("q6textarea").value = review.cmt6;
+		document.getElementById("q7textarea").value = review.cmt7;
+		document.getElementById("q8textarea").value = review.cmt8;
 	}
 
 
-	var reviewUpdate = function (team, q1, cmt1, q2, cmt2, q3, cmt3, q4, cmt4, q5, cmt5) {
+	var reviewUpdate = function (team, q1, cmt1, q2, cmt2, q3, cmt3, q4, cmt4, q5, cmt5, q6, cmt6, q7, cmt7, q8, cmt8) {
 		if (q1 != 0) {
 			team.update({q1: q1});
 		}
@@ -128,8 +142,17 @@ angular
 		if (q4 != 0) {
 			team.update({q4: q4});
 		}
-		if (q5 != undefined) {
+		if (q5 != 0) {
 			team.update({q5: q5});
+		}
+		if (q6 != 0) {
+			team.update({q6: q6});
+		}
+		if (q7 != 0) {
+			team.update({q7: q7});
+		}
+		if (q8 != undefined) {
+			team.update({q8: q8});
 		}
 		if (cmt1 != "") {
 			team.update({cmt1: cmt1});
@@ -146,8 +169,17 @@ angular
 		if (cmt5 != "") {
 			team.update({cmt5: cmt5});
 		}
-		if (q1 != 0 && q2 != 0 && q3 != 0 && q4 != 0) {
-			var teamavg = (parseFloat(q1) + parseFloat(q2) + parseFloat(q3) + parseFloat(q4))/4.0;
+		if (cmt6 != "") {
+			team.update({cmt6: cmt6});
+		}
+		if (cmt7 != "") {
+			team.update({cmt7: cmt7});
+		}
+		if (cmt8 != "") {
+			team.update({cmt8: cmt8});
+		}
+		if (q1 != 0 && q2 != 0 && q3 != 0 && q4 != 0 && q5 != 0 && q6 != 0 && q7 != 0) {
+			var teamavg = (parseFloat(q1) + parseFloat(q2) + parseFloat(q3) + parseFloat(q4) + parseFloat(q5) + parseFloat(q6) + parseFloat(q7))/7.0;
 			teamavg = teamavg.toFixed(2);
 			team.update({teamavgval: teamavg});
 		}
@@ -210,14 +242,57 @@ angular
 		}
 	});
 
-
+	$("#q5slider").slider({
+		min: 0,
+		step: 1,
+		max: 7,
+		value: 0
+	}).each(function() {
+		var opt = $(this).data().uiSlider.options;
+		var vals = opt.max - opt.min;
+		for (var i = 0; i <= vals; i++) {
+			var el = $('<label>' + (i + opt.min) + '</label>').css('left', (i/vals*100) + '%');
+			$("#q5slider").append(el);
+		}
+	});
+	
+	$("#q6slider").slider({
+		min: 0,
+		step: 1,
+		max: 7,
+		value: 0
+	}).each(function() {
+		var opt = $(this).data().uiSlider.options;
+		var vals = opt.max - opt.min;
+		for (var i = 0; i <= vals; i++) {
+			var el = $('<label>' + (i + opt.min) + '</label>').css('left', (i/vals*100) + '%');
+			$("#q6slider").append(el);
+		}
+	});
+	
+	$("#q7slider").slider({
+		min: 0,
+		step: 1,
+		max: 7,
+		value: 0
+	}).each(function() {
+		var opt = $(this).data().uiSlider.options;
+		var vals = opt.max - opt.min;
+		for (var i = 0; i <= vals; i++) {
+			var el = $('<label>' + (i + opt.min) + '</label>').css('left', (i/vals*100) + '%');
+			$("#q7slider").append(el);
+		}
+	});
 
 	var calcAvg = function(team) {
-		var q1, q2, q3, q4;
+		var q1, q2, q3, q4, q5, q6, q7;
 		var q1sum = 0;
 		var q2sum = 0;
 		var q3sum = 0;
 		var q4sum = 0;
+		var q5sum = 0;
+		var q6sum = 0;
+		var q7sum = 0;
 		var teamavg = 0;
 
 		var reviewArray = $firebaseArray(team.child('reviews'));
@@ -228,19 +303,28 @@ angular
 				q2sum += parseFloat(reviewArray[i].q2);
 				q3sum += parseFloat(reviewArray[i].q3);
 				q4sum += parseFloat(reviewArray[i].q4);
+				q5sum += parseFloat(reviewArray[i].q5);
+				q6sum += parseFloat(reviewArray[i].q6);
+				q7sum += parseFloat(reviewArray[i].q7);
 			};
 
 			q1 = q1sum/reviewArray.length;
 			q2 = q2sum/reviewArray.length;
 			q3 = q3sum/reviewArray.length;
 			q4 = q4sum/reviewArray.length;
+			q5 = q5sum/reviewArray.length;
+			q6 = q6sum/reviewArray.length;
+			q7 = q7sum/reviewArray.length;
 
-			teamavg = (q1 + q2 + q3 + q4)/4.0;
+			teamavg = (q1 + q2 + q3 + q4 + q5 + q6 + q7)/7.0;
 
 			q1 = q1.toFixed(2);
 			q2 = q2.toFixed(2);
 			q3 = q3.toFixed(2);
 			q4 = q4.toFixed(2);
+			q5 = q5.toFixed(2);
+			q6 = q6.toFixed(2);
+			q7 = q7.toFixed(2);
 			teamavg = teamavg.toFixed(2);
 
 			team.update({
@@ -248,6 +332,9 @@ angular
 				q2Val: q2,
 				q3Val: q3,
 				q4Val: q4,
+				q5Val: q5,
+				q6Val: q6,
+				q7Val: q7,
 				ovrAvg: teamavg
 			});
 
@@ -262,9 +349,12 @@ angular
 		var q2sum = 0;
 		var q3sum = 0;
 		var q4sum = 0;
+		var q5sum = 0;
+		var q6sum = 0;
+		var q7sum = 0;
 		var ovrsum = 0;
 
-		var q1, q2, q3, q4, ovr;
+		var q1, q2, q3, q4, q5, q6, q7, ovr;
 
 		var teamsArray = $firebaseArray(team.parent());
 
@@ -274,6 +364,9 @@ angular
 				q2sum+= parseFloat(teamsArray[i].q2Val);
 				q3sum+= parseFloat(teamsArray[i].q3Val);
 				q4sum+= parseFloat(teamsArray[i].q4Val);
+				q5sum+= parseFloat(teamsArray[i].q5Val);
+				q6sum+= parseFloat(teamsArray[i].q6Val);
+				q7sum+= parseFloat(teamsArray[i].q7Val);
 				ovrsum+= parseFloat(teamsArray[i].ovrAvg);
 			};
 
@@ -281,12 +374,18 @@ angular
 			q2 = q2sum/teamsArray.length;
 			q3 = q3sum/teamsArray.length;
 			q4 = q4sum/teamsArray.length;
+			q5 = q5sum/teamsArray.length;
+			q6 = q6sum/teamsArray.length;
+			q7 = q7sum/teamsArray.length;
 			ovr = ovrsum/teamsArray.length;
 
 			q1 = q1.toFixed(2);
 			q2 = q2.toFixed(2);
 			q3 = q3.toFixed(2);
 			q4 = q4.toFixed(2);
+			q5 = q5.toFixed(2);
+			q6 = q6.toFixed(2);
+			q7 = q7.toFixed(2);
 			ovr = ovr.toFixed(2);
 
 			team.parent().parent().child("averages").update({
@@ -294,28 +393,31 @@ angular
 				q2avg: q2,
 				q3avg: q3,
 				q4avg: q4,
+				q5avg: q5,
+				q6avg: q6,
+				q7avg: q7,
 				ovrAvg: ovr
 			});
 
 		});
 	}
 
-	var checkZero = function(q1, q2, q3, q4, q5) {
-		if (q1 != 0 && q2 != 0 && q3 != 0 && q4 != 0 && q5 != undefined) {
+	var checkZero = function(q1, q2, q3, q4, q5, q6, q7, q8) {
+		if (q1 != 0 && q2 != 0 && q3 != 0 && q4 != 0 && q5 != 0 && q6 != 0 && q7 != 0 && q8 != undefined) {
 			return false;
 		}
 		return true;
 	}
 
-	var isCmtsBlank= function(cmt1, cmt2, cmt3, cmt4, cmt5) {
-		if(cmt1 == "" && cmt2 == "" && cmt3 == "" && cmt4 == "" && cmt5 == ""){
+	var isCmtsBlank= function(cmt1, cmt2, cmt3, cmt4, cmt5, cmt6, cmt7, cmt8) {
+		if(cmt1 == "" && cmt2 == "" && cmt3 == "" && cmt4 == "" && cmt5 == "" && cmt6 == "" && cmt7 == "" && cmt8 == ""){
 	    	return true;
 	    }
 	    return false;
 	}
 
 	class Evaluation {
-	    constructor(user, teamName, q1, cmt1, q2, cmt2, q3, cmt3, q4, cmt4, q5, cmt5) {
+	    constructor(user, teamName, q1, cmt1, q2, cmt2, q3, cmt3, q4, cmt4, q5, cmt5, q6, cmt6, q7, cmt7, q8, cmt8) {
 		    this.user = user;
 		    this.teamName = teamName;
 		    this.q1 = q1;
@@ -328,7 +430,13 @@ angular
 		    this.cmt4 = cmt4;
 		    this.q5 = q5;
 		    this.cmt5 = cmt5;
-				this.rank = -1;
+			this.q6 = q6;
+		    this.cmt6 = cmt6;
+		    this.q7 = q7;
+		    this.cmt7 = cmt7;
+		    this.q8 = q8;
+		    this.cmt8 = cmt8;
+			this.rank = -1;
 	    }
 	}
 
@@ -339,13 +447,19 @@ angular
 		q2 = $("#q2slider").slider("option", "value");
 		q3 = $("#q3slider").slider("option", "value");
 		q4 = $("#q4slider").slider("option", "value");
-		q5 = $('input[name="q5radio"]:checked').val();
+		q5 = $("#q5slider").slider("option", "value");
+		q6 = $("#q6slider").slider("option", "value");
+		q7 = $("#q7slider").slider("option", "value");
+		q8 = $('input[name="q8radio"]:checked').val();
 
 		cmt1 = document.getElementById("q1textarea").value;
 		cmt2 = document.getElementById("q2textarea").value;
 		cmt3 = document.getElementById("q3textarea").value;
 		cmt4 = document.getElementById("q4textarea").value;
 		cmt5 = document.getElementById("q5textarea").value;
+		cmt6 = document.getElementById("q6textarea").value;
+		cmt7 = document.getElementById("q7textarea").value;
+		cmt8 = document.getElementById("q8textarea").value;
 
 		// graderName = document.getElementById("grader-name-input").value;
 		graderName = $rootScope.user;
@@ -360,7 +474,7 @@ angular
 	    //@TODO: implemented user auth
 	    var user = graderName;
 
-	    var evaluation = new Evaluation(user, curTeamName, q1, cmt1, q2, cmt2, q3, cmt3, q4, cmt4, q5, cmt5);
+	    var evaluation = new Evaluation(user, curTeamName, q1, cmt1, q2, cmt2, q3, cmt3, q4, cmt4, q5, cmt5, q6, cmt6, q7, cmt7, q8, cmt8);
 	    var submit_alert;
 
 
@@ -394,17 +508,17 @@ angular
 		        	}
 		        	if (userExists) {
 		        		reviewToEdit = reviews.child(reviewID);
-		        		reviewUpdate(reviewToEdit, q1, cmt1, q2, cmt2, q3, cmt3, q4, cmt4, q5, cmt5);
+		        		reviewUpdate(reviewToEdit, q1, cmt1, q2, cmt2, q3, cmt3, q4, cmt4, q5, cmt5, q6, cmt6, q7, cmt7, q8, cmt8);
 		        		calcAvg(team);
 								redirect();
 		        	}
 		        	else {
 		        		//check that (no radios) || (all comments) == null
-		        		if (checkZero(q1, q2, q3, q4, q5)) { //not all radios completed
+		        		if (checkZero(q1, q2, q3, q4, q5, q6, q7, q8)) { //not all radios completed
 		        			//throw an error
 				        	$("#slide-text2").slideDown();
 		        		}
-		        		else if (isCmtsBlank(cmt1, cmt2, cmt3, cmt4, cmt5)) { //no comments
+		        		else if (isCmtsBlank(cmt1, cmt2, cmt3, cmt4, cmt5, cmt6, cmt7, cmt8)) { //no comments
 		        			$("#slide-text2").hide();
 							submit_alert = confirm("You didn't type in any comments/ Are you sure you want to submit the form?");
 		        			if (submit_alert) {
