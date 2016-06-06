fcfe322c20a65a78880373ab9a717ee17caf2c14
Yoni Pinto <yoni.b.pinto@gmail.com>
2016-06-06 17:43:03 -0500

    changed team comments view, added class average to teamview

--- app/team/team.js
+++ app/team/team.js
@@ -17,35 +17,94 @@ angular
       }
     }
 
+    var ref = new Firebase($rootScope.sessionRef);
     var teamRef = new Firebase($rootScope.sessionRef + "/teams/" + $rootScope.teamID);
     var team = new $firebaseObject(teamRef);
 
     var reviewRef = new Firebase($rootScope.sessionRef + "/teams/" + $rootScope.teamID + "/reviews");
     $scope.reviews = $firebaseArray(reviewRef);
 
-    $scope.avg = {};
+    $scope.avg = $firebaseArray(ref.child("averages"));
+
+    $scope.cmts1 = [];
+    $scope.cmts2 = [];
+    $scope.cmts3 = [];
+    $scope.cmts4 = [];
+    $scope.cmts5 = [];
+    $scope.cmts6 = [];
+    $scope.cmts7 = [];
+    $scope.cmts8 = [];
+
+    $scope.teamAvg = {};
     team.$loaded(function() {
       $rootScope.teamName = team.name;
-      $scope.avg.q1 = team.q1Val;
-      $scope.avg.q2 = team.q2Val;
-      $scope.avg.q3 = team.q3Val;
-      $scope.avg.q4 = team.q4Val;
-      $scope.avg.q5 = team.q5Val;
-      $scope.avg.q6 = team.q6Val;
-      $scope.avg.q7 = team.q7Val;
+      $scope.teamAvg.q1 = team.q1Val;
+      $scope.teamAvg.q2 = team.q2Val;
+      $scope.teamAvg.q3 = team.q3Val;
+      $scope.teamAvg.q4 = team.q4Val;
+      $scope.teamAvg.q5 = team.q5Val;
+      $scope.teamAvg.q6 = team.q6Val;
+      $scope.teamAvg.q7 = team.q7Val;
+      $scope.teamAvg.q8 = team.q8Val;
+      $scope.teamAvg.ovrAvg = team.ovrAvg;
     });
 
-    $scope.check = function(str) {
-      if (str!="") return true;
-      else return false;
+
+    var check = function(str) {
+      if (str != "") {
+        return true;
+      }
+      return false;
     }
+
     $scope.reviews.$loaded(function() {
       $scope.reviews.forEach(function(review) {
-        if (review.q8=="1") {
-          review.continue = "Yes";
+
+        console.log(review.cmt1);
+        console.log(check(review.cmt1));
+
+        if (check(review.cmt1)) {
+          console.log("pushing");
+          $scope.cmts1.push(review.user + ": " + review.cmt1);
+          console.log("pushed");
         }
-        else {
-          review.continue = "No";
+
+        if (check(review.cmt2)) {
+          $scope.cmts2.push(review.user + ": " + review.cmt2);
+        }
+       
+        if (check(review.cmt3)) {
+          $scope.cmts3.push(review.user + ": " + review.cmt3);
+        } 
+
+        if (check(review.cmt4)) {
+          $scope.cmts4.push(review.user + ": " + review.cmt4);
+        }
+
+        if (check(review.cmt5)) {
+          $scope.cmts5.push(review.user + ": " + review.cmt5);
+        }
+
+        if (check(review.cmt6)) {
+          $scope.cmts6.push(review.user + ": " + review.cmt6);
+        }
+
+        if (check(review.cmt7)) {
+          $scope.cmts7.push(review.user + ": " + review.cmt7);
+        }
+
+        if (check(review.cmt8)) {
+          $scope.cmts8.push(review.user + ": " + review.cmt8);
+        }
+
+        var sum = (parseFloat(review.q1) + parseFloat(review.q2) + parseFloat(review.q3) + parseFloat(review.q4) + parseFloat(review.q5) + parseFloat(review.q6) + parseFloat(review.q7));
+        review.ovr = sum/7.0;
+        review.ovr = (review.ovr).toFixed(2);
+ 
+        if (review.q8 == 1) {
+          review.cont = "Yes";
+        } else {
+          review.cont = "No";
         }
 
       });
